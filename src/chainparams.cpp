3aed2dbe6699a68451fc80ee2c467148f7851135
Watercoolertje <watercoolertje@gmail.com>
2017-01-10 18:04:29 +0100

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -129,7 +129,7 @@ public:
         vSeeds.push_back(CDNSSeedData("bbaynode (node11)", "85.25.146.74"));
 
 
-	base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,25);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,25);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,85);
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,153);
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();

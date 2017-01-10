1288beeed3952dbeca971baade5db1432b6215ad
Watercoolertje <watercoolertje@gmail.com>
2017-01-10 18:02:49 +0100

    Fix boost related compile error
    Found fix for code at rat4/blackcoin repository

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -128,12 +128,12 @@ public:
         vSeeds.push_back(CDNSSeedData("bbaynode (node10)", "69.254.222.98"));
         vSeeds.push_back(CDNSSeedData("bbaynode (node11)", "85.25.146.74"));
 
- 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,25);
+
+	base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,25);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,85);
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,153);
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
-        base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<uns
+        base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
         convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
 

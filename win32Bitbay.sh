cad5f6d5bc7301b3f8113f8afcb7f6a6b11aa607
yshurik <yshurik@gmail.com>
2018-01-06 11:04:46 +0100

    Compile mxe mingw (#15)
    * Adjustements for building bitbayd on mxe/mingw
    
    * Compiling with old gcc 4.9.2, different symbols references in boost with -std=c++, use -DBOOST_NO_CXX11_SCOPED_ENUMS, fixes to makeqt-linux.sh
    
    * Compiling with old gcc 4.9.2, use -DBOOST_NO_CXX11_SCOPED_ENUMS for bitbayd (makefile.unix)

--- win32Bitbay.sh
+++ win32Bitbay.sh
@@ -39,7 +39,9 @@ function reset
     
     sudo apt-get update || force "apt-get update failed"
 
-    sudo apt-get install -y mxe-i686-w64-mingw32.static-boost mxe-i686-w64-mingw32.static-miniupnpc mxe-i686-w64-mingw32.static-openssl mxe-i686-w64-mingw32.static-db mxe-i686-w64-mingw32.static-zlib mxe-i686-w64-mingw32.static-libpng || force "apt-get install failed"
+    sudo apt-get install -y mxe-i686-w64-mingw32.static-boost mxe-i686-w64-mingw32.static-miniupnpc mxe-i686-w64-mingw32.static-openssl mxe-i686-w64-mingw32.static-zlib mxe-i686-w64-mingw32.static-libpng || force "apt-get install failed"
+	sudo apt-get install -y mxe-i686-w64-mingw32.static-gcc || force "apt-get install failed"
+	sudo apt-get remove -y mxe-i686-w64-mingw32.static-db || force "apt-get remove failed"
 
     rm -rf "$BASE_DIR"
     mkdir -p "$BASE_DIR"
@@ -48,17 +50,40 @@ function reset
     git clone https://github.com/dzimbeck/bitbay
 
     cd bitbay
+    echo "*** db-4.8 ***"
+    curl -fsSL -o db-4.8.30.NC.tar.gz https://github.com/bitbaymarket/bitbay-prebuilt-libs1/releases/download/base1/db-4.8.30.NC.tar.gz
+	curl -fsSL -o db-4.8.30-fix-for-mingw.patch https://github.com/bitbaymarket/bitbay-prebuilt-libs1/raw/master/patches/db-4.8.30-fix-for-mingw.patch
+    tar -zxf db-4.8.30.NC.tar.gz
+    mv db-4.8.30.NC db
+    cd db
+    cd build_unix
+	export MXE_PATH=/usr/lib/mxe
+	export CC=$MXE_PATH/usr/bin/i686-w64-mingw32.static-gcc 
+    export CXX=$MXE_PATH/usr/bin/i686-w64-mingw32.static-g++ 
+    sh ../dist/configure --enable-mingw --enable-cxx -host x86 -disable-replication --prefix=$MXE_PATH/usr/i686-w64-mingw32.static
+    perl -i -pe 's/define socklen_t int/define socklen_t_xx int/m' db_config.h
+    perl -i -pe 's/define HAVE_REPLICATION_THREADS 1/define HAVE_REPLICATION_THREADS_XX 1/m' db_config.h
+	cd ..
+    patch -p1 < ../db-4.8.30-fix-for-mingw.patch
+	cd build_unix
+    make -j2
+    cd ..
+    cd ..
+    mkdir static_db_4_8_win
+    mkdir static_db_4_8_win/include
+    cp db/build_unix/*.h static_db_4_8_win/include/
+    cp db/build_unix/lib*a static_db_4_8_win/
 
-    git config --global user.name "Your Name"
-    git config --global user.email "you@example.com"    
-    git am "$PATCH_PATH"
+    #git config --global user.name "Your Name"
+    #git config --global user.email "you@example.com"    
+    #git am "$PATCH_PATH"
 
     #cd src/leveldb
     #chmod 755 build_detect_platform
     #make
     #make libmemenv.a
-    cp LevelDB/libleveldb.a src/leveldb/libleveldb.a
-    cp LevelDB/libmemenv.a src/leveldb/libmemenv.a
+    #cp LevelDB/libleveldb.a src/leveldb/libleveldb.a
+    #cp LevelDB/libmemenv.a src/leveldb/libmemenv.a
 }
 
 function build
